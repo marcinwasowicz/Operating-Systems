64a94368d7a8458acd6adb7f2dc9989062247978
marcinwasowicz <48218867+marcinwasowicz@users.noreply.github.com>
2020-03-09 17:59:19 +0100

    Add files via upload

---
+++ DiffOperationsLibrary.h
@@ -0,0 +1,69 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+#include<stdarg.h>
+#include<stdio.h>
+
+char* susbtr(char* string, int start, int length);
+
+char* stringConcat(int num, ...);
+
+char* readTextFileToString(char* fileName);
+
+char* makeSysCall(char* name1, char* name2, char* outputFile);
+
+typedef struct{
+    char* name1;
+    char* name2;
+}filesPair;
+
+filesPair* makeFilesPair(char* n1, char* n2);
+
+void deletePair(filesPair* pair);
+
+typedef struct{
+    char** operations;
+    int numberOfOperations;
+    int rawSize;
+}operationsBlock;
+
+operationsBlock* makeBlock(char** operations, int numberOfOperations);
+
+void printOneBlockOperation(operationsBlock* block, int index);
+
+void printBlock(operationsBlock* block);
+
+void deleteBlock(operationsBlock* block);
+
+typedef struct{
+    operationsBlock** arr;
+    filesPair** sequence;
+    int rawSize;
+    int head;
+}blockArray;
+
+bool isDigit(char c);
+
+bool isThisBeginingOfOperations(char* string, int index);
+
+operationsBlock* parseOperationsStream(char* stream);
+
+operationsBlock* diffPair(filesPair* pair);
+
+blockArray* makeBlockArrayFromScratch(int number, ...);
+
+void printArray(blockArray* block);
+
+void deleteBlockArray(blockArray* arr);
+
+void deleteOneBlockFromArray(blockArray* arr, int index);
+
+void deleteSingleOperation(operationsBlock* block, int index);
+
+void addOneBlock(blockArray* bl, char* fileName1, char* filename2);
+
+#endif
